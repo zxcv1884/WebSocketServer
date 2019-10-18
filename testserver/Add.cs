559ece3287292be4a834d7ec1159676429f8027f
zxcv1884 <39883679+zxcv1884@users.noreply.github.com>
2019-10-18 17:04:26 +0800

    first commit

---
+++ testserver/Add.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace testserver
+{
+    class Add : WebSocketBehavior
+    {
+        protected override void OnOpen()
+        {
+            Console.WriteLine("Connection Open");
+            base.OnOpen();
+        }
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            var data = e.Data;
+            if (TestJson(data))
+            {
+                var param = JToken.Parse(data);
+                if (param["a"] != null && param["b"] != null)
+                {
+                    var a = param["a"].ToObject<int>();
+                    var b = param["b"].ToObject<int>();
+                    Send(JsonConvert.SerializeObject(new { code = 200, msg = "result is " + (a + b) }));
+                    //Task.Factory.StartNew(() => {
+                    //    Task.Delay(10000).Wait();
+                    //    Send(JsonConvert.SerializeObject(new { code = 200, msg = "I just to tell you, the connection is different from http, i still alive and could send message to you." }));
+                    //});
+                }
+            }
+            else
+            {
+                Send(JsonConvert.SerializeObject(new { code = 400, msg = "request is not a json string." }));
+            }
+        }
+
+        protected override void OnClose(CloseEventArgs e)
+        {
+            Console.WriteLine("Connection Closed");
+            base.OnClose(e);
+        }
+
+        protected override void OnError(ErrorEventArgs e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+            base.OnError(e);
+        }
+
+        private static bool TestJson(string json)
+        {
+            try
+            {
+                JToken.Parse(json);
+                return true;
+            }
+            catch (JsonReaderException ex)
+            {
+                Console.WriteLine(ex);
+                return false;
+            }
+        }
+    }
+}
---
+++ testserver/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using WebSocketSharp.Server;
+
+namespace testserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wssv = new WebSocketServer(4649);
+            wssv.AddWebSocketService<Add>("/add");
+            wssv.Start();
+            Console.WriteLine("Server starting, press any key to terminate the server.");
+            Console.ReadKey(true);
+            wssv.Stop();
+        }
+    }
+}
